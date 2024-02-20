edd2974b26abf0dd8bdb212497bd824983c6702b
AJIS2AI <159204520+AJIS2AI@users.noreply.github.com>
2024-02-20 21:40:39 +0900

    Create breadth-first-search.js

---
+++ breadth-first-search.js
@@ -0,0 +1,26 @@
+const graph = {
+'E' : ['D', 'A'],
+'F' : ['D'],
+'A' : ['E', 'C', 'B'],
+'B' : ['A'],
+'C' : ['A'],
+'D' : ['E', 'F']
+};
+
+function width(graph, start) {
+  let visited = [];
+  let queue = [start];
+  while (queue.length !== 0) {
+    let n = queue.shift();
+    if (!visited.includes(n)) {
+      visited.push(n);
+      let sub = graph[n].filter(x => !visited.includes(x));
+      for (let i of sub) {
+        queue.push(i);
+      }
+    }
+  }
+  return visited;
+}
+
+console.log(width(graph, 'E'));
