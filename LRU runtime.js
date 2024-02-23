217c0ad40d4794837f50bca93c95a4db8d5fbdea
Songhui An <159204520+AJIS2AI@users.noreply.github.com>
2024-02-23 22:30:31 +0900

    Create LRU runtime.js

---
+++ LRU runtime.js
@@ -0,0 +1,31 @@
+function LRU(frame, page) {
+  let runTime = 0;
+  let temp = [];
+
+  if (frame == 0) {
+    runTime = page.length * 6;
+    return runTime;
+  }
+
+  for (let i of page) {
+    if (temp.includes(i)) {
+      temp.splice(temp.indexOf(i), 1);
+      temp.push(i);
+      runTime += 1;
+    } else {
+      if (temp.length < frame) {
+        temp.push(i);
+      } else {
+        temp.shift();
+        temp.push(i);
+      }
+      runTime += 6;
+    }
+  }
+  return runTime;
+}
+
+const f = +prompt("Please input frame");
+const page = prompt("Please input page.").split("");
+
+console.log(LRU(f, page));
